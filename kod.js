6e81d948435d078d0934180e88a3068212bd6e76
Oniz <Oniz2062@gmail.com>
2023-05-15 21:18:40 +0400

    ужасы

--- kod.js
+++ kod.js
@@ -1,3 +1,11 @@
+//эл. управления карты
+var traffic;
+chbox=document.getElementById('traffic');
+var kolesiko;
+chbox=document.getElementById('kolesiko');
+var dvigKart;
+chbox=document.getElementById('dvigKart');
+//
 var marker=[];
 var coordsmark=[];
 var namepopup=[];
@@ -5,27 +13,36 @@ var coordsmarks=JSON.parse(localStorage.getItem("coordsmark"));
 var namepopups=JSON.parse(localStorage.getItem("namepopup"));
 var nameway=[];
 var sohrway=[];
+var sohrways=JSON.parse(localStorage.getItem("sohrway"));
+var nameways=JSON.parse(localStorage.getItem("nameway"));
 //перенос данных
 if(coordsmarks!=null){
     coordsmark=coordsmarks;
     namepopup=namepopups;
 }
+if(sohrways!=null){
+    sohrway=sohrways;
+    nameway=nameways;
+}
+console.log(sohrway); console.log(nameway); console.log(coordsmark); console.log(namepopup);
 //
-var map;
-var polyline;
-var unikline;
 var coords=[];
 var p=0;
 var w=0;
-DG.then(function () {
-    map = DG.map('map', {
+var map;
+var unikline;
+var polyline;
+DG.then(function(){
+        map = DG.map('map', {
         center: [53.208763, 50.124508],
+        dragging: false,
         zoom: 20,
         touchzoom: true,
-        scrollwheelzoom: true,
+        zoomControl: true,
+        scrollWheelZoom: false,
+        doubleClickZoom: false,
     });
-    //
-    map.on('click', function(e) {
+        map.on('click', function(e) {
         coords.push([e.latlng.lat ,e.latlng.lng]);  
         if (!polyline){
         polyline = DG.polyline(coords, {
@@ -35,22 +52,13 @@ DG.then(function () {
         polyline.setLatLngs(coords).redraw();
     }
         });
+
 });
-for(i=0;i<=3;i++){
-    if(namepopup[i]==''){
-    break
-    }
-    else{
-        var marker = DG.marker(coordsmark[i])
-        .addTo(map).bindPopup(''+namepopup[i]+'');
-    }
-};
 // Сохранение и удаление маркера
 function sohrMarker(){
     coordsmark[p]=coords[0];
     namepopup[p]=prompt('Введите название Маркера!',''+p+'');
-    marker[p] = DG.marker(coordsmark[p])
-    .addTo(map).bindPopup(''+namepopup[p]+'');
+    marker[p] = DG.marker(coordsmark[p]).addTo(map).bindPopup(''+namepopup[p]+'');
     coords=[];
     p+=1;
 };
@@ -67,36 +75,67 @@ coords.pop(),
 polyline.setLatLngs(coords).redraw();
     });
 };
-function sohr_way(){       
-w+=1; 
-nameway[w]= prompt('Введите название маршрута!',''+w+'');
-sohrway[w]=coords;
-DG.then(function(){
+var sohr_way=()=>{      
+    w+=1; 
+    nameway[w]= prompt('Введите название маршрута!',''+w+'');
+    sohrway[w]=coords;
     unikline=DG.polyline(sohrway[w],{color:'red'}).bindLabel(nameway[w]).addTo(map);
-    console.log(sohrway[w],nameway[w]);
-});
-coords=[];
-CreateWayButton(); 
-};
-// кнопка для пути
-function CreateWayButton(){
-var button = document.createElement("input");
-button.type = "button";
-button.value = ''+w+'';
-button.onclick = function zoombutn(){
-map.flyTo(sohrway[button.value][1], 18, {
-animate: true,
-duration: 1.5
-                });
-};
-document.body.append(button);
+    coords=[];
 };
 //
-function sohr(){
+function save(){
     //сохранение маркеров
     localStorage.setItem("coordsmark", JSON.stringify(coordsmark));
     localStorage.setItem("namepopup", JSON.stringify(namepopup));
     //сохранение путей
     localStorage.setItem("sohrway", JSON.stringify(sohrway));
     localStorage.setItem("nameway", JSON.stringify(nameway));
+    //сохранение
 };
+function clear(){
+    localStorage.clear();
+};
+//загрузка
+function load(map){
+while(coordsmarks!=null){
+    marker[p] = DG.marker(coordsmark[p])
+    .addTo(map).bindPopup(''+namepopup[p]+'');
+    p+=1;
+}
+while(nameways!=''){
+    w+=1;
+    unikline=DG.polyline(sohrway[w],{color:'red'}).bindLabel(nameway[w]).addTo(map);
+}
+};
+//управелние значениями карты
+function traffic() {
+    var traffic;
+    traffic=document.getElementById('traffic');
+        if (traffic.checked) {
+            alert('Выбран');
+        }
+        else {
+            alert ('Не выбран');
+        }
+    }
+function kolesiko() {
+    var kolesiko;
+    kolesiko=document.getElementById('kolesiko');
+        if (kolesiko.checked) {
+            map.scrollWheelZoom.enable();
+        }
+        else {
+            map.scrollWheelZoom.disable();
+        }
+    }
+function dvigKart() {
+    var dvigKart;
+    dvigKart=document.getElementById('dvigKart');
+        if (dvigKart.checked) {
+            map.dragging.enable();
+        }
+        else {
+            map.dragging.disable();;
+        }
+}
+//
